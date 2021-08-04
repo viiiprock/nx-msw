74f7febd3b2e79aba58a35ba505f4ba8b38546d5
Son Dang <son.dang@doda100.com>
2021-08-05 04:15:41 +0700

    mispelling

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,27 +1,30 @@
-import useSWR from 'swr'
-import { request } from "graphql-request";
+import useSWR from 'swr';
+import { request } from 'graphql-request';
 
 const graphqlQuery = `{
   GetUser: {
-    name
+    user: {
+      name
+    }
   }
-}`
+}`;
 
-const fetcher = (url) => fetch(url).then((res) => res.json())
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export function Index() {
-  const { data } = useSWR('http://localhost:3333/user', fetcher)
-  
-  const { data: graphqlData  } = useSWR(graphqlQuery, (query) =>
+  const { data } = useSWR('http://localhost:3333/user', fetcher);
+
+  const { data: graphqlData } = useSWR(graphqlQuery, (query) =>
     request('http://localhost:3333/graphql', query)
   );
 
-
-  return <div>
-    With Rest API: {data?.name}
-    <br/>
-    With Graphql API: {graphqlData?.user.naem}
-  </div>
+  return (
+    <div>
+      With Rest API: {data?.name}
+      <br />
+      With Graphql API: {graphqlData?.user.name}
+    </div>
+  );
 }
 
 export default Index;
